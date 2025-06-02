fd7705fa141c1cc325506c8e52b312d0ceaf12ea
Sweet Van Loan <Sweet@SweetVanLoan.com>
2025-06-02 17:16:51 -0500

    Update ProfileScreen.js

--- screens/screens/ProfileScreen.js
+++ screens/screens/ProfileScreen.js
@@ -1,1 +1,23 @@
+import React, { useContext } from 'react';
+import { View, Text, Image } from 'react-native';
+import { AuthContext } from '../AuthProvider';
+
+const ProfileScreen = () => {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <View>
+      <Text>Profile</Text>
+      {user && (
+        <View>
+          <Image source={{ uri: user.photoURL }} style={{ width: 100, height: 100 }} />
+          <Text>Name: {user.displayName}</Text>
+          <Text>Email: {user.email}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default ProfileScreen;
 
