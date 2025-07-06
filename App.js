bcbaf2f75a1a7075654cc867fecbe0b5a84183c1
Sweet Van Loan <Sweet@SweetVanLoan.com>
2025-07-06 15:43:04 -0500

    Update App.js
    Looping in new imports

--- App.js
+++ App.js
@@ -5,6 +5,31 @@ import { AuthProvider } from './AuthProvider';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import { setDoc, doc } from 'firebase/firestore';
+import { auth, db } from './firebase';
+
+const registerForPushNotificationsAsync = async () => {
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') return;
+
+    const token = (await Notifications.getExpoPushTokenAsync()).data;
+
+    // Save token to Firestore
+    const user = auth.currentUser;
+    if (user) {
+      await setDoc(doc(db, 'users', user.uid), { expoPushToken: token }, { merge: true });
+    }
+  }
+};
+
 
 const Stack = createNativeStackNavigator();
 
