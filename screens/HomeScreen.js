ccbe4e21268a4a289827861272c727392dc9fb48
Sweet Van Loan <Sweet@SweetVanLoan.com>
2025-09-02 09:18:44 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { registerForPushNotificationsAsync } from './utils/notificationUtils';
+
 
 const HomeScreen = () => {
   const navigation = useNavigation();
